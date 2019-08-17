a1a4ee05ca1d88404638062498e46e90126b1d8c
zagwalker <34241512+zagwalker@users.noreply.github.com>
2019-08-17 20:06:08 +0300

    Update exercise6-fix.sh

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+if [ "$#" -ne 2 ]; then
+    echo "Please provide at least 2 arguments. Last argument is the destination folder on the remote server"
+    exit 1
+fi
+
+if [ $HOSTNAME == server1 ]; then
+	remote=server2
+	else
+	remote=server1
+fi
+
+dest_dir=${@: -1}
+array=( "$@" )
+	unset "array[${#array[@]}-1]"
+	for i in "${array[@]}"; do
+		scp $i vagrant@$remote:/$dest_dir
+		files_size=$(wc -c "$i" | awk '{print $1}')
+		let "total_copy_size += $files_size"
+	done
+	echo $total_copy_size
