5954e7f55f7ac0ef6e371f27b46a6ba204db79b3
zagwalker <34241512+zagwalker@users.noreply.github.com>
2019-08-17 20:05:15 +0300

    Update exercise5-fix_server1.sh

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-#add fix to exercise5-server1 here
+echo "Host server?" >> /etc/ssh/ssh_config
+echo "    StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+echo "    UserKnownHostsFile=/dev/null" >> /etc/ssh/ssh_config
+
+su - vagrant -c 'ssh-keygen -f ~/.ssh/id_rsa -P ""'
+
+grep "vagrant@server1" /home/vagrant/.ssh/id_rsa.pub  > /vagrant/server1Key
