2b949f3b7b03a471502a4488433f06f7f7e7e43e
zagwalker <34241512+zagwalker@users.noreply.github.com>
2019-08-17 20:03:53 +0300

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-#add fix to exercise3 here
+for i in {1..1}; do sed -i '/deny from all/d' /etc/apache2/sites-enabled/000-default ; done
+for i in {1..1}; do sed -i s/"allow,deny"/"deny,allow"/g /etc/apache2/sites-enabled/000-default ; done
+/etc/init.d/apache2 restart
